be7cafd403a40e8635b612808c8e604e47ccb4cb
Daniel Abib <danielsabib@gmail.com>
2022-05-20 19:17:38 -0300

    Fisrt working impl.

--- frame-getter.py
+++ frame-getter.py
@@ -1,15 +1,32 @@
 import os
 import subprocess
+from typing import Optional
 
 import typer
 
 app = typer.Typer()
 
-files = os.listdir('videos/')
-print(files)
-for file in files:
-    query = f'ffmpeg -i videos/{file} -vf fps=1/5 -s 960x420 frames/{file}-out%04d.jpg'
-    subprocess.run(query, shell=True)
+def main(input: Optional[str] = typer.Argument(None),
+         fps:float = 1/5,
+         size: str = "960x540") -> str:
+    if input:
+        query = f'ffmpeg -i {input} -r {fps} -s {size} frames/TEST-out%04d.jpg'
+        subprocess.run(query, shell=True)
+        raise typer.Exit()
+
+    files = os.listdir('videos/')
+    if not files:
+        typer.echo("Videos folder is empty.\nInsert a video in the videos folder or specify an input.")
+        raise typer.Exit(code=1)
+
+    for file in files:
+        if size:
+            query = f'ffmpeg -i videos/{file} -r {fps} -s {size} frames/{file}-out%04d.jpg'
+        else:
+            query = f'ffmpeg -i videos/{file} -r {fps} frames/{file}-out%04d.jpg'
+        subprocess.run(query, shell=True)
+    raise typer.Exit()
 
 if __name__ == '__main__':
-    app()
+    # app()
+    typer.run(main)
